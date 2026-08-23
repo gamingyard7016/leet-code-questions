d12e3e8be399d3c25dd1c40a75a22cc1274dbf6d
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-08-23 17:08:59 +0530

    Time: 4 ms (33.88%), Space: 9.7 MB (49.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,8 +4,7 @@ public:
         if(s.length() != t.length()) return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        if(s == t) return true;
-        else return false;
+        return(s==t);
         
     }
 };
