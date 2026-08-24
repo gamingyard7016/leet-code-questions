89166794fd0a9992af8b5b77ca069ec61755bcfb
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-08-25 00:49:59 +0530

    Time: 0 ms (100%), Space: 26.1 MB (70.19%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-        if(n==1) return arr[0];
-        if(arr[0] != arr[1]) return arr[0];
-        if(arr[n-1] != arr[n-2]) return arr[n-1];
-        int lo = 0, hi = n-1;
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
-            int f = mid, s = mid; // f is first mid, s is second mid
-            if(arr[mid] == arr[mid-1]) f = mid - 1;
-            else s = mid + 1;
-            if((f-lo) % 2 == 1) hi = f - 1; // f-lo is left side length
-            else lo = s + 1;
+    int singleNonDuplicate(vector<int>& nums) {
+
+        int lo = 0;
+        int hi = nums.size() - 1;
+
+        while(lo < hi) {
+
+            int mid = lo + (hi - lo) / 2;
+
+            if(mid % 2 == 1) {
+                mid--;
+            }
+
+            if(nums[mid] == nums[mid + 1]) {
+                lo = mid + 2;
+            }
+            else {
+                hi = mid;
+            }
         }
-        return 8875;
+
+        return nums[lo];
     }
 };
