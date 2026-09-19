2e0d49a6662cd3d44b1e8deef9aa77c3ac7655f5
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-09-20 04:49:12 +0530

    Time: 3 ms (78.42%), Space: 54.8 MB (44.85%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+
+        // Final answer store karne ke liye
         vector<int> ans;
-        vector<int> tmp(nums.size()+1);
-        for(int x:nums){
+
+        // Har number ka count store karenge
+        vector<int> tmp(nums.size() + 1);
+
+        // nums ke har number ko count karo
+        for(int i = 0; i < nums.size(); i++) {
+
+            int x = nums[i];
+
             tmp[x]++;
         }
-        for(int i=1; i<=nums.size();i++){
-            if(tmp[i] == 0){
+
+        // 1 se n tak check karo
+        for(int i = 1; i <= nums.size(); i++) {
+
+            // Agar count 0 hai,
+            // matlab number array mein nahi mila
+            if(tmp[i] == 0) {
+
                 ans.push_back(i);
             }
         }
+
         return ans;
     }
 };
