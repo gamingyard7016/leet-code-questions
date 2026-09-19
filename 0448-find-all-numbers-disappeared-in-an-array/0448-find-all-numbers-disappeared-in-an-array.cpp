3658f0c2656a64fc3dc12670c8e7d71a90a8db1a
Subham kumar <110382359+gamingyard7016@users.noreply.github.com>
2026-09-20 05:11:10 +0530

    Time: 0 ms (100%), Space: 54.7 MB (46.88%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -4,9 +4,7 @@ public:
         vector<int>newvector (nums.size()+1);
         vector<int>ans;
         for(int i=0;i<nums.size();i++) newvector[nums[i]]++;
-        for(int i=1;i<=nums.size();i++){
-            if(newvector[i] == 0) ans.push_back(i);
-        }
+        for(int i=1;i<=nums.size();i++) if(newvector[i] == 0) ans.push_back(i);
         return ans;
     }
 };
